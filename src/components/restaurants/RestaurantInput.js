cdbb7aa6c15e783037505ec6ffd4ca741edc2db2
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-12 02:20:45 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+    constructor(props) {
+      super(props);
 
   state = {
     text: ''
   }
-
+}
     handleOnChange = event => {
       this.setState({
         text: event.target.value,
