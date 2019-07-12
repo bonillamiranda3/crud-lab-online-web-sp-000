30bf65f3cff974275ff43a8ee1381732719e7a81
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-12 02:37:34 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
+import { connect } from 'react-redux'
 
 class ReviewsContainer extends Component {
 
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+      <ReviewInput
+         addReview={this.props.addReview}
+         restaurantId={this.props.restaurant.id} />
+       <Reviews
+         reviews={this.props.reviews}
+         restaurantId={this.props.restaurant.id}
+         deleteReview={this.props.deleteReview}/>
+      
       </div>
     )
   }
