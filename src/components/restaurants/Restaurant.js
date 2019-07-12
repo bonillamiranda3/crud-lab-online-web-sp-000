5f9aa13a1a9ca998dfc7e7e9b5f2cbb6ee2ac958
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-12 02:18:51 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
-
+  
+  handleOnClick() {
+    this.props.deleteRestaurant(this.props.restaurant.id);
+  }
 
   render() {
     const { restaurant } = this.props;
