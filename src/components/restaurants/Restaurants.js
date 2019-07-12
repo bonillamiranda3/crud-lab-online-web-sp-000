f7442e36878da95da71f42c18d26cd708cd70895
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-12 02:41:10 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import Restaurant from './Restaurant';
 
-class Restaurants extends Component {
-  render() {
-    const ( restaurants, deleteRestaurant) = this.props;
-    const restaurantList = restaurants.map(restaurant = {
-    return(
-      <Restaurant
-           key={restaurant.id}
-           restaurant={restaurant}
-           deleteRestaurant={deleteRestaurant}
-       />
-     )
-   });
+import Restaurant from './Restaurant';
 
-   return(
-     <ul>
-       //Restaurants Component
-       {restaurantList}
-     </ul>
-   );
- }
+class Restaurants extends Component {
+
+  render() {
+    const { restaurants, deleteRestaurant } = this.props;
+    const restaurantList = restaurants.map(restaurant => {
+      return (
+        <Restaurant
+            key={restaurant.id}
+            restaurant={restaurant}
+            deleteRestaurant={deleteRestaurant}
+        />
+      )
+    });
+
+    return(
+      <ul>
+        //Restaurants Component
+        {restaurantList}
+      </ul>
+    );
+  }
 };
 
 export default Restaurants;
